71e5fe294b8f8a4fe5ef8d7f88e2e16400e324cd
Bryan Alejandro Cubillos Prieto <bryan.cubillos@bizagi.com>
2025-04-01 07:29:06 -0500

    MOB: Add single scroll view

--- chat_app/lib/pages/login_page.dart
+++ chat_app/lib/pages/login_page.dart
@@ -13,18 +13,24 @@ class LoginPage extends StatelessWidget {
     return Scaffold(
       backgroundColor: Color(0xffF2F2F2),
       body: SafeArea(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Logo(),
-            _Form(),
-            Labels(),
-            Text('Términos y condiciones de uso',
-              style: TextStyle(
-                fontWeight: FontWeight.w200
-              )
+        child: SingleChildScrollView(
+          physics: BouncingScrollPhysics(),
+          child: SizedBox(
+            height: MediaQuery.of(context).size.height * 0.9,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Logo(),
+                _Form(),
+                Labels(),
+                Text('Términos y condiciones de uso',
+                  style: TextStyle(
+                    fontWeight: FontWeight.w200
+                  )
+                ),
+              ],
             ),
-          ],
+          ),
         ),
       )
     );
