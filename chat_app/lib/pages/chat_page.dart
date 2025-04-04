f05adb61435b8bef681b8e1e8ea07073d4112ea7
Bryan Alejandro Cubillos Prieto <bryant_alejandro@hotmail.com>
2025-04-04 09:33:45 -0500

    UI:MOB: add chat functions

--- chat_app/lib/pages/chat_page.dart
+++ chat_app/lib/pages/chat_page.dart
@@ -1,18 +1,107 @@
+import 'dart:io';
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class ChatPage extends StatelessWidget {
+class ChatPage extends StatefulWidget {
   const ChatPage({super.key});
 
+  @override
+  State<ChatPage> createState() => _ChatPageState();
+}
+
+class _ChatPageState extends State<ChatPage> {
+  final TextEditingController _textController = TextEditingController();
+  final FocusNode _focusNode = FocusNode();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Chat'),
-        centerTitle: true,
+        backgroundColor: Colors.white,
+        title: Column(
+          children: <Widget>[
+            CircleAvatar(
+              backgroundColor: Colors.blueAccent[100],
+              child: Text('Te', style: TextStyle(fontSize: 12)),
+              maxRadius: 14,
+            ),
+            SizedBox(height: 3),
+            Text('Melisa Florez', style: TextStyle(color: Colors.black87, fontSize: 14))
+          ],
+        )
       ),
-      body: const Center(
-        child: Text('Chat Page'),
+      body: Container(
+        child: Column(
+          children: <Widget>[
+            
+            Flexible(
+              child: ListView.builder(
+                physics: BouncingScrollPhysics(),
+                itemBuilder: (_, i) => Text('$i'),
+                reverse: true,
+              ),
+            ),
+
+            Divider(
+              height: 1,
+            ),
+
+            // Input chat
+            _inputChat()
+          ],
+        )
+      )
+    );
+  }
+
+  Widget _inputChat() {
+    return SafeArea(
+      child: Container(
+        margin: EdgeInsets.symmetric(horizontal: 8.0),
+        color: Colors.white,
+        height: 100,
+        child: Row(
+          children: <Widget>[
+            Flexible(
+              child: TextField(
+                controller: _textController,
+                onSubmitted: _handleSummit,
+                onChanged: (_) {
+                  // TODO:Cuando hay un valor para poder postear
+                },
+                decoration: InputDecoration.collapsed(
+                  hintText: 'Enviar mensaje',
+                  border: InputBorder.none,
+                  hintStyle: TextStyle(color: Colors.black54)
+                ),
+                focusNode: _focusNode,
+              ),
+            ),
+            // send button
+            Container(
+              margin: EdgeInsets.symmetric(horizontal: 4.0),
+              child: !Platform.isIOS
+                ? CupertinoButton(
+                  child: Text('Enviar', style: TextStyle(color: Colors.blueAccent[400])),
+                  onPressed: () {},
+                )
+                : Container(
+                  margin: EdgeInsets.symmetric(horizontal: 4.0),
+                  child: IconButton(
+                    icon: Icon(Icons.send), color: Colors.blueAccent[400],
+                    onPressed: () {},
+                  ),
+                ),
+            )
+          ],
+        ),
       ),
     );
+  }
+
+  _handleSummit(String text) {
+    print(text);
+    _textController.clear();
+    _focusNode.requestFocus();
   }
 }
